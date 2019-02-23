496d2e158df6fd056390bf407d9fbf6716eea489
Darcy Linde <lindedarcy@gmail.com>
2019-02-23 23:42:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = -1.0/0
+  min_value = 1.0/0
   name_hash.each do |key,value|
-    if value > min_value
+    if value < min_value
       min_key = key
       min_value = value
   end
